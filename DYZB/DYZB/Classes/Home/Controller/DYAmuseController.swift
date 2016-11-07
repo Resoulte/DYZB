8dcd2e0c5563536f4424f881867912bbf01477f1
Resolute <shifei2013_ios@163.com>
2016-11-07 15:35:38 +0800

    amusecontroller继承baseviewcontroller

--- DYZB/DYZB/Classes/Home/Controller/DYAmuseController.swift
+++ DYZB/DYZB/Classes/Home/Controller/DYAmuseController.swift
@@ -8,103 +8,26 @@
 
 import UIKit
 
-// MARK: -定义常量
-private let kMargain : CGFloat = 10
-private let kItemW = (kScreenW - kMargain * 3) / 2
-private let kNormalItemH = kItemW * 3/4
-private let kPrettyItemH = kItemW * 4/3
-private let kHeaderH : CGFloat = 50
-private let kGameViewH : CGFloat = 90.0
-private let kNormalID = "normalID"
-private let kPrettyID = "prettyID"
-private let kHeaderID = "headerID"
-
-
-class DYAmuseController: UIViewController {
+class DYAmuseController: DYBaseViewController {
 
     // MARK: - 懒加载
     lazy var amuseVM = DYAmuseViewModel()
-    lazy var collection: UICollectionView = { [weak self] in
-        
-        let flow = UICollectionViewFlowLayout()
-        flow.itemSize = CGSize(width: kItemW, height: kNormalItemH)
-        flow.minimumLineSpacing = 0
-        flow.minimumInteritemSpacing = kMargain
-        flow.headerReferenceSize = CGSize(width: kScreenW, height: kHeaderH)
-        // 设置内边距
-        flow.sectionInset = UIEdgeInsets(top: 0, left: kMargain, bottom: 0, right: kMargain)
-        
-        
-        let collection = UICollectionView(frame: (self?.view.bounds)!, collectionViewLayout: flow)
-        collection.backgroundColor = UIColor.white
-        collection.dataSource = self
-        collection.delegate = self
-        
-        // MARK: - 根据父控件的宽高自动调整
-        collection.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        collection.register(UINib(nibName: "DYNormalCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: kNormalID)
-        
-        collection.register(UINib(nibName: "DYPrettyCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: kPrettyID)
-        
-        collection.register(UINib(nibName: "DYHeaderCollectionReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderID)
-        
-        return collection
-        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // 设置UI
-        setupUI()
-        
-        // 加载数据
-        loadData()
-    }
-
-    
-
-}
-
-// MARK: - 设置UI布局
-extension DYAmuseController {
-    func setupUI() {
-        // 添加collection
-        view.addSubview(collection)
     }
 }
+
 
 //  MARK: - 加载数据
 extension DYAmuseController {
-    func loadData() {
+   override func loadData() {
+    // 1.给父类中的viewModel赋值
+        baseVM = amuseVM
+    // 2.请求数据
         amuseVM.loadAmuseData {
             self.collection.reloadData()
         }
     }
 }
 
-// MARK: - UICollectionViewDataSource
-extension DYAmuseController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return amuseVM.anchorGroups.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return amuseVM.anchorGroups[section].anchors.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalID, for: indexPath) as! DYNormalCollectionViewCell
-        
-        cell.anchor = self.amuseVM.anchorGroups[indexPath.section].anchors[indexPath.item]
-//        cell.backgroundColor = UIColor.randomColor()
-        return cell
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kHeaderID, for: indexPath) as! DYHeaderCollectionReusableView
-        header.group = amuseVM.anchorGroups[indexPath.section]
-        
-        return header
-    }
-}
---
+++ DYZB/DYZB/Classes/Main/Controller/DYBaseViewController.swift
@@ -0,0 +1,110 @@
+//
+//  DYBaseViewController.swift
+//  DYZB
+//
+//  Created by ma c on 16/11/7.
+//  Copyright © 2016年 shifei. All rights reserved.
+//
+
+import UIKit
+
+// MARK: -定义常量
+private let kMargain : CGFloat = 10
+private let kItemW = (kScreenW - kMargain * 3) / 2
+private let kNormalItemH = kItemW * 3/4
+private let kPrettyItemH = kItemW * 4/3
+private let kHeaderH : CGFloat = 50
+private let kGameViewH : CGFloat = 90.0
+private let kNormalID = "normalID"
+private let kPrettyID = "prettyID"
+private let kHeaderID = "headerID"
+
+
+class DYBaseViewController: UIViewController {
+
+    // MARK: - 定义属性
+    var baseVM : DYBaseViewModel!
+    lazy var collection: UICollectionView = { [weak self] in
+        
+        let flow = UICollectionViewFlowLayout()
+        flow.itemSize = CGSize(width: kItemW, height: kNormalItemH)
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = kMargain
+        flow.headerReferenceSize = CGSize(width: kScreenW, height: kHeaderH)
+        // 设置内边距
+        flow.sectionInset = UIEdgeInsets(top: 0, left: kMargain, bottom: 0, right: kMargain)
+        
+        
+        let collection = UICollectionView(frame: (self?.view.bounds)!, collectionViewLayout: flow)
+        collection.backgroundColor = UIColor.white
+        collection.dataSource = self
+        collection.delegate = self
+        
+        // MARK: - 根据父控件的宽高自动调整
+        collection.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        collection.register(UINib(nibName: "DYNormalCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: kNormalID)
+        
+        collection.register(UINib(nibName: "DYPrettyCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: kPrettyID)
+        
+        collection.register(UINib(nibName: "DYHeaderCollectionReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeaderID)
+        
+        return collection
+        }()
+    
+    // MARK: - 系统回调
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 设置UI
+        setupUI()
+        
+        // 加载数据
+        loadData()
+    }
+
+}
+
+//  MARK: - 加载数据
+extension DYBaseViewController {
+    func loadData() {
+                
+    }
+}
+
+// MARK: - 设置UI布局
+extension DYBaseViewController {
+    func setupUI() {
+        // 添加collection
+        view.addSubview(collection)
+    }
+}
+
+
+// MARK: - UICollectionViewDataSource
+extension DYBaseViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return baseVM.anchorGroups.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return baseVM.anchorGroups[section].anchors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalID, for: indexPath) as! DYNormalCollectionViewCell
+        
+        cell.anchor = baseVM.anchorGroups[indexPath.section].anchors[indexPath.item]
+        //        cell.backgroundColor = UIColor.randomColor()
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kHeaderID, for: indexPath) as! DYHeaderCollectionReusableView
+        header.group = baseVM.anchorGroups[indexPath.section]
+        
+        return header
+    }
+}
+
