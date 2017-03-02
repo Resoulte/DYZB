bef9baf37c52d1603e09b352d026f8a08e661329
师飞 <shifei@shifeideMacBook-Pro.local>
2017-03-03 01:40:29 +0800

    “趣玩界面的基本布局”

---
+++ DYZB/DYZB/Classes/Home/Controller/DYFunnyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DYFunnyViewController.swift
+//  DYZB
+//
+//  Created by 师飞 on 2017/3/1.
+//  Copyright © 2017年 shifei. All rights reserved.
+//
+
+import UIKit
+
+class DYFunnyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
